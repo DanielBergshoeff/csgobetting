21a6ad50a23d223501d22e0bbe9df51bd54ef6c3
Bergshoeff,Daniel D.A <d.bergshoeff@student.fontys.nl>
2016-08-09 13:44:06 +0200

    player scraping fixed and working

--- Structured/HTMLscraping/HTMLscrapingPlayers.py
+++ Structured/HTMLscraping/HTMLscrapingPlayers.py
@@ -18,8 +18,8 @@ import requests
 
 # INSERT NEEDED INFORMATION HERE
 playerIDsOnHLTV = [
-    695, #allu
-    9216 #coldzera
+    [695, 'allu'],
+    [9216, 'coldzera']
 ]
 #linkToPlayerPage = 'http://www.hltv.org/?pageid=246&playerid='
 
@@ -35,9 +35,9 @@ with open(root+ 'D:\Daniel\Github\CSGOBetting\Structured\idFiles\playerIDs.txt',
 with open(root+ 'D:\Daniel\Github\CSGOBetting\Structured\idFiles\mapIDs.txt', "r") as text_file:
     mapIDs = text_file.readline().split()
     
-for i in range(0,len(playerIDsOnHLTV)):
+for l in range(0,len(playerIDsOnHLTV)):
     linkToPlayerPage = 'http://www.hltv.org/?pageid=246&playerid='
-    linkToPlayerPage += str(playerIDsOnHLTV[i])
+    linkToPlayerPage += str(playerIDsOnHLTV[l][0])
     
     # download page
     page = requests.get(linkToPlayerPage)
@@ -45,7 +45,7 @@ for i in range(0,len(playerIDsOnHLTV)):
     
     # this will create a list of the extracted text
     datesAndTeams = tree.xpath('//div[@class="covSmallHeadline"]/a/text()')
-    rest = tree.xpath('//div[@class="covSmallHeadline"]/text()')
+    rest = tree.xpath('//div[@class="covSmallHeadline"]//text()')
     
     # create lists
     dates = list()
@@ -68,24 +68,21 @@ for i in range(0,len(playerIDsOnHLTV)):
     rating = list()
     
     # split and process collected list to individual lists
-    for i in range(0,len(datesAndTeams)):
-        currentI = (i-9) % 3
+    for i in range(10,len(rest)):
+        currentI = (i - 10) % 9
         if (currentI == 0):
-            dates.append(datesAndTeams[i].replace(" ","/20"))
+            dates.append(rest[i].replace(" ","/20"))
         if (currentI == 1):
-            homeTeam.append(datesAndTeams[i].replace(" ",""))
+            homeTeam.append(rest[i].replace(" ",""))
         if (currentI == 2):
-            outTeam.append(datesAndTeams[i].replace(" ",""))
-            
-    for i in range(0, len(rest)):
-        currentI = (i-9) % 5
-        if (currentI == 0):
-            map.append(rest[i])
-        if (currentI == 1):
-            kills.append(rest[i])
+            outTeam.append(rest[i].replace(" ",""))
         if (currentI == 3):
-            deaths.append(rest[i])
+            map.append(rest[i])
         if (currentI == 4):
+            kills.append(rest[i])
+        if (currentI == 6):
+            deaths.append(rest[i])
+        if (currentI == 8):
             rating.append(rest[i])
             
     # split date
@@ -100,29 +97,23 @@ for i in range(0,len(playerIDsOnHLTV)):
         homeTeamScore.append(homeTeam[i].split("(")[1].replace(")",""))
         outTeamName.append(outTeam[i].split("(")[0])
         outTeamScore.append(outTeam[i].split("(")[1].replace(")",""))
-    
-    # convert names to IDs
-    homeTeamIds = list()
-    outTeamIds = list ()
-    playedMapIDs = list ()
-    for i in range(0,len(homeTeam)):
-        if (homeTeamName[i] in teamIDs):
-            homeTeamIds.append(teamIDs.index(homeTeamName[i]))
-    
-    for i in range(0,len(outTeam)):
-        if (outTeamName[i] in teamIDs):
-            outTeamIds.append(teamIDs.index(outTeamName[i]))
-        else:
-            teamIDs.append(outTeamName[i])
-            outTeamIds.append(teamIDs.index(outTeamName[i]))
-    
-    for i in range(0,len(map)):
-        if (map[i] in mapIDs):
-            playedMapIDs.append(mapIDs.index(map[i]))
-        else:
-            mapIDs.append(map[i])
-            playedMapIDs.append(mapIDs.index(map[i]))
-        
-    # printing tests    
-    print(homeTeamName)
-    
+            
+    # write everything to file
+    playerName = playerIDsOnHLTV[l][1]
+    playerName += '.txt'
+    filename = 'D:\Daniel\Github\CSGOBetting\Structured\playerMatchFiles\\matches_'
+    filename += playerName
+    with open(filename, "w+") as text_file:
+        for i in range(0,len(dates)):
+            text_file.write("{0},{1},{2}, {3}, {4}, {5}, {6}, {7}, {8}, {9}, {10} \n".format(
+                date_year[i], str(date_month[i]).zfill(2), str(date_day[i]).zfill(2),
+                str(homeTeamName[i]).zfill(3),
+                str(homeTeamScore[i]).zfill(2),
+                str(outTeamName[i]).zfill(3),
+                str(outTeamScore[i]).zfill(2),
+                str(map[i]).zfill(2),
+                str(kills[i]).zfill(2),
+                str(deaths[i]).zfill(2),
+                str(rating[i]).zfill(3)))                
+
+print('DONE')    

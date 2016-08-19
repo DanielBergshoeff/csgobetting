d0af76d34fbf4c1553d29b8a4b3777253d3868ab
Bergshoeff,Daniel D.A <d.bergshoeff@student.fontys.nl>
2016-08-19 15:04:43 +0200

    Player scraping completed

--- Structured/testingScripts/testPath.py
+++ Structured/testingScripts/testPath.py
@@ -34,9 +34,9 @@ sys.path.append('/usr/local/lib/python2.7/site-packages')
 from lxml import html
 import requests
 
-allPlayersHLTV = [
-    ['coldzera', 9216] 
-]
+import csv
+import numpy as np
+import matplotlib.pyplot as plt
 
 # import ID files
 if platform == "darwin":
@@ -55,24 +55,48 @@ elif platform == "win32" or platform == "cygwin":
         mapIDs = text_file.readline().split()
     with open(root+ 'idFiles\\eventIDs.txt', "r") as text_file:
         eventIDs = text_file.readline().split()
-        
-    
-for l in range(0,len(allPlayersHLTV)):
-    linkToPlayerPage = 'http://www.hltv.org/?pageid=175&playerid='
-    linkToPlayerPage += str(allPlayersHLTV[l][1])
-    
-    # download page
-    page = requests.get(linkToPlayerPage)
-    tree = html.fromstring(page.content)
-    
-    # this will create a list of the extracted text
-    rest = tree.xpath('//div[@class="covSmallHeadline"]//text()')
 
+linkToMatchesPage = 'http://www.hltv.org/match/2304000-virtuspro-nip-esl-pro-league-season-4-europe'
+# download page
+page = requests.get(linkToMatchesPage)
+tree = html.fromstring(page.content)
     
-    # split and process collected list to individual lists
-    for x in range(0, 25):
-        x = 5
+# this will create a list of the extracted text    
+rest = tree.xpath('//div[@class="text-center"]//text()')
+
+players = [rest[1], rest[3], rest[5], rest[7], rest[9], rest[11], rest[13], rest[15], rest[17], rest[19]]
+import_data = list()
+
+print(players[3])
+
+for i in range(0, len(players)):
+    importfile = root + "playerMatchFiles\\matches_"
+    playerfile =  players[i].replace(" ", "") + ".txt"
+    matchfile = importfile + playerfile
+
+    import_data.append(np.genfromtxt(matchfile, delimiter = ','))
+    
+import_dates = import_data[0][:,][:,0:3]
+import_times = import_data[0][:,][:,3]
+import_homeTeamID = import_data[0][:,][:,4]
+import_outTeamID = import_data[0][:,][:,5]
+import_mapID = import_data[0][:,][:,6]
+import_eventID = import_data[0][:,][:,7]
+import_homeTeamScore = import_data[0][:,][:,8]
+import_outTeamScore = import_data[0][:,][:,9]
+import_homeTeamCTRounds = import_data[0][:,][:,10]
+import_outTeamTRounds = import_data[0][:,][:,11]
+import_homeTeamTRounds = import_data[0][:,][:,12]
+import_outTeamCTRounds = import_data[0][:,][:,13]
+import_playerTeamID = import_data[0][:,][:,14]
+import_playerKills = import_data[0][:,][:,15]
+import_playerDeaths = import_data[0][:,][:,16]
+import_playerRating = import_data[0][:,][:,17]
+
+print(import_playerRating[0])
+
+'''plt.plot(import_playerKills, import_playerDeaths)
+plt.show()'''
+
         
-    print(rest)
-            
-    
+print('DONE')
